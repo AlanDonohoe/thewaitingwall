0eb0074f57b48b4c81aea02c61119fddbce2c628
Alan Donohoe <alandonohoe@hotmail.com>
2015-03-21 19:26:21 +0000

    style form and nav bar

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,19 +1,19 @@
 $(function() {
   $(".the-what-link").click(function() {
-    $(this).parent().addClass('active');
-    $(this).parent().siblings().removeClass('active');
-    $("#main-content").replaceWith("<div id=\"main-content\"><h3>The What?</h3><p>Based on Jerusalem's Waling Wall, where since the 4th Century people have anonymously posted their prayers of hope, concerns or sought forgiveness by confessing their wrong-doings.</p></div>");
+    // $(this).parent().addClass('active');
+    // $(this).parent().siblings().removeClass('active');
+    $("#main-content").replaceWith("<div id=\"main-content\"><h3>The What?</h3><p>Based on Jerusalem's Waling Wall, where since the 4th Century people have anonymously posted their prayers of hope, concerns or sought forgiveness by confessing their wrong-doings... etc etc...</p></div>");
   });
 
   $(".the-why-link").click(function() {
-    $(this).parent().addClass('active');
-    $(this).parent().siblings().removeClass('active');
-    $("#main-content").replaceWith("<div id=\"main-content\"><h3>The Why?</h3><p>In a secular world....</p></div>");
+    // $(this).parent().addClass('active');
+    // $(this).parent().siblings().removeClass('active');
+    $("#main-content").replaceWith("<div id=\"main-content\"><h3>The Why?</h3><p>In a secular world....... etc etc....</p></div>");
   });
 
   $(".the-who-link").click(function() {
-    $(this).parent().addClass('active');
-    $(this).parent().siblings().removeClass('active');
-    $("#main-content").replaceWith("<div id=\"main-content\"><h3>The Who?</h3><p>Free The Trees and Alain de Botton</p></div>");
+    // $(this).parent().addClass('active');
+    // $(this).parent().siblings().removeClass('active');
+    $("#main-content").replaceWith("<div id=\"main-content\"><h3>The Who?</h3><p>Free The Trees and Alain de Botton... etc etc....</p></div>");
   });
 });
