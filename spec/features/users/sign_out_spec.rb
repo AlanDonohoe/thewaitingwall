7c875850a0ff4ba0a4cc3ab56625b1c4fc5cf6d4
Alan Donohoe <alandonohoe@hotmail.com>
2015-03-22 18:28:27 +0000

    fix broken specs

--- spec/features/users/sign_out_spec.rb
+++ spec/features/users/sign_out_spec.rb
@@ -12,7 +12,8 @@ feature 'Sign out', :devise do
     user = FactoryGirl.create(:user)
     signin(user.email, user.password)
     expect(page).to have_content I18n.t 'devise.sessions.signed_in'
-    click_link 'Sign out'
+    save_and_open_page
+    click_link('Sign out', match: :first)
     expect(page).to have_content I18n.t 'devise.sessions.signed_out'
   end
 
--- spec/features/visitors/navigation_spec.rb
+++ spec/features/visitors/navigation_spec.rb
@@ -8,11 +8,28 @@ feature 'Navigation links', :devise do
   #   Given I am a visitor
   #   When I visit the home page
   #   Then I see "home," "sign in," and "sign up"
-  scenario 'view navigation links' do
-    visit root_path
-    expect(page).to have_content 'Home'
+  scenario 'admin user views devise navigation links, but not the ability to sign up' do
+    visit new_user_session_path
     expect(page).to have_content 'Sign in'
-    expect(page).to have_content 'Sign up'
+    expect(page).to_not have_content 'Sign up'
+    expect(page).to_not have_content 'The What?'
+    expect(page).to_not have_content 'The Why?'
+  end
+
+  scenario 'regular user does not see devise navigation links' do
+    visit root_path
+    expect(page).to_not have_content 'Sign in'
+    expect(page).to_not have_content 'Sign up'
+    expect(page).to have_content 'The What?'
+    expect(page).to have_content 'The Why?'
+  end
+
+  scenario 'visiting the wall user does not see navigation links' do
+    visit messages_path
+    expect(page).to_not have_content 'Sign in'
+    expect(page).to_not have_content 'Sign up'
+    expect(page).to_not have_content 'The What?'
+    expect(page).to_not have_content 'The Why?'
   end
 
 end
