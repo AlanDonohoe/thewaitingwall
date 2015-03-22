b7656ebc42a7d3ee97fe6cb8b9ee506a4d2483c3
Alan Donohoe <alandonohoe@hotmail.com>
2015-03-22 18:23:15 +0000

    add mailcatcher settings to env dev.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,15 +27,20 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.mandrillapp.com",
-    port: 587,
-    domain: Rails.application.secrets.domain_name,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_apikey
-  }
+  # mailcatcher:
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  #  - - - -- 
+  # and comment out this block:
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.mandrillapp.com",
+  #   port: 587,
+  #   domain: Rails.application.secrets.domain_name,
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: Rails.application.secrets.email_provider_username,
+  #   password: Rails.application.secrets.email_provider_apikey
+  # }
+
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
