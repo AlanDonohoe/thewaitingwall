3fed2b32bc12661119581772083a49190453d586
Alan Donohoe <alandonohoe@hotmail.com>
2015-04-06 17:52:39 +0100

    speed up scroll to animation

--- app/assets/javascripts/scrolling_and_animations.js
+++ app/assets/javascripts/scrolling_and_animations.js
@@ -4,12 +4,11 @@ $(function() {
     $("#flapper-display-area").show();
     $('html, body').animate({
       scrollTop: $("#flapper-display-area").offset().top
-    }, 2000);
+    }, 700);
   });
   $("#edit_message").click(function() {
-    // $("#flapper-display-area").show();
     $('html, body').animate({
       scrollTop: $(".nav-wrapper").offset().top
-    }, 1000);
+    }, 500);
   });
 });
