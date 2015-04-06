76c4556d591c390705458fab73167e21a0db79f7
Alan Donohoe <alandonohoe@hotmail.com>
2015-04-06 18:00:54 +0100

    hide btn on click

--- app/assets/javascripts/scrolling_and_animations.js
+++ app/assets/javascripts/scrolling_and_animations.js
@@ -1,12 +1,14 @@
 $(function() {
   $("#flapper-display-area").hide();
   $("#message_review").click(function() {
+    $("#message_review").hide();
     $("#flapper-display-area").show();
     $('html, body').animate({
       scrollTop: $("#flapper-display-area").offset().top
     }, 700);
   });
   $("#edit_message").click(function() {
+    $("#message_review").show();
     $('html, body').animate({
       scrollTop: $(".nav-wrapper").offset().top
     }, 500);
