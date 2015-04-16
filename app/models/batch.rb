c002d337c10c9bba2e0862fe1be612ed070427af
Alan Donohoe <alandonohoe@hotmail.com>
2015-04-16 18:58:31 +0100

    implement basic batch functionality

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,3 +1,21 @@
 class Batch < ActiveRecord::Base
+  MAX_NO_OF_LETTERS_IN_BATCH = 200
   has_many :messages
+  after_create :collect_new_messages
+
+  def collect_new_messages
+    total_no_of_letters = 0
+    Message.approved_messages.limit(10).each do |message|
+      total_no_of_letters += message.message_text.length
+      break if total_no_of_letters > MAX_NO_OF_LETTERS_IN_BATCH
+      incremented_times_shown = message.times_shown + 1
+      message.update_attributes(batch_id: self.id, times_shown: incremented_times_shown)
+    end
+  end
+
+  def letter_count
+    total_no_of_letters = 0
+    messages.each {|message| total_no_of_letters +=  message.message_text.length }
+    total_no_of_letters
+  end
 end
