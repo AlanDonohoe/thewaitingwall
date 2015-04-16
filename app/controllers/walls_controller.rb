56206bee3c0f8d71ac4e228e434884a5a5114702
Alan Donohoe <alandonohoe@hotmail.com>
2015-04-16 18:57:47 +0100

    remove null object pattern and just create first batch if no batches exist

--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -1,6 +1,6 @@
 class WallsController < ApplicationController
   before_action :set_wall, only: [:show]
-  before_action :get_current_batch_of_messages, only: [:show]
+  before_action :get_current_batch, only: [:show]
 
   # GET /walls/1
   # GET /walls/1.json
@@ -11,7 +11,7 @@ class WallsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_wall
-      @wall = Wall.first # was: params[:id] - but like Highlander, there can be only one wall.
+      @wall = Wall.first
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
@@ -19,7 +19,7 @@ class WallsController < ApplicationController
       params[:wall]
     end
 
-    def get_current_batch_of_messages
-      @current_batch_of_messages = Message.approved_messages
+    def get_current_batch
+      @current_batch = Batch.last || Batch.create
     end
 end
