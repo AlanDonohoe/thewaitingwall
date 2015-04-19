eb5e13729cfb386ed304a079846db084fcd3e19c
Alan Donohoe <alandonohoe@hotmail.com>
2015-04-19 09:41:53 +0100

    typo

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-config.assets.paths << Rails.root.join('app', 'assets', 'font')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'font')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
