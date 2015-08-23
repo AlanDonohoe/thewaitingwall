82a3aab67db37cc5aa4a5af25099dcb6fe4dd806
Alan Donohoe <alandonohoe@hotmail.com>
2015-08-23 09:09:05 +0100

    add new where tab and reinstate wall_layout for main wall view

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -2,8 +2,8 @@
   $(function(){
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $("li.last a").click(function(){
-      window.location.href = "http://www.thewaitingwall.com/";
-    }); 
+    // $("li.last a").click(function(){
+    //   window.location.href = "http://www.thewaitingwall.com/";
+    // }); 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -13,31 +13,31 @@ class WallsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_wall
-      @wall = Wall.first
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_wall
+    @wall = Wall.first
+  end
 
-    def set_show_link
-      show_link = params.permit(:show_link).present? ? true : false
-      @link = {}
-      if show_link
-        @link[:link_text] = 'thewaitingwall.com'
-        @link[:link_anchor] = 'http://www.thewaitingwall.com'
-        @link[:wrapper_class] = "col s2 offset-s10"
-      else
-        @link[:link_text] = 'write message'
-        @link[:link_anchor] = 'messages/new'
-        @link[:wrapper_class] = "col s2 offset-s10"
-      end
+  def set_show_link
+    show_link = params.permit(:show_link).present? ? true : false
+    @link = {}
+    if show_link
+      @link[:link_text] = 'thewaitingwall.com'
+      @link[:link_anchor] = 'http://www.thewaitingwall.com'
+      @link[:wrapper_class] = "col s2 offset-s10"
+    else
+      @link[:link_text] = 'write message'
+      @link[:link_anchor] = 'messages/new'
+      @link[:wrapper_class] = "col s2 offset-s10"
     end
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def wall_params
-      params.permit(:show_link)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def wall_params
+    params.permit(:show_link)
+  end
 
-    def get_current_batch
-      @current_batch = Batch.last || Batch.create
-    end
+  def get_current_batch
+    @current_batch = Batch.last || Batch.create
+  end
 end
