842c8f70120bfd1219d886040f7a0bcfa62ca35a
Alan Donohoe <alandonohoe@hotmail.com>
2015-08-23 22:32:19 +0100

    add petes latest changes - repsonsive parallax images, etc

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,9 +1,28 @@
-(function($){
+(function($){ 
   $(function(){
+    
+  // media queries image swap
+    var imgsmall = "(min-device-width:320px)";
+    var imgbig   = "(min-device-width:960px)";
+    function imageresize() {
+      if(window.matchMedia(imgbig).matches) {
+        $('.change img').each(function () {
+            this.src= "/assets/background2.jpg";
+        });
+      }
+      else if(window.matchMedia(imgsmall).matches) {
+        $('.change img').each(function () {
+            this.src= "/assets/background1.jpg";
+        });
+      }
+    }
+
+    imageresize();
+
+    $(window).bind("resize", function() {
+        imageresize();
+    });
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    // $("li.last a").click(function(){
-    //   window.location.href = "http://www.thewaitingwall.com/";
-    // }); 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
