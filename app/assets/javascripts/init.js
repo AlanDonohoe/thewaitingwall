ddb5ce2dc349cf07ea0a53c9408df88a134402ff
Alan Donohoe <alandonohoe@hotmail.com>
2015-09-03 04:36:09 +0100

    move wall background image to be a background of body element

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -19,19 +19,14 @@
     var imgbig   = "(min-device-width:960px)";
     function imageresize() {
       if(window.matchMedia(imgbig).matches) {
-        $('.change img').each(function () {
-            this.src= "/assets/background2-6492c83fc5943157d9debcd80089e7f7.jpg";
-        });
+        $('.full-screen-img').css("background-image", "url(/assets/background2-6492c83fc5943157d9debcd80089e7f7.jpg) no-repeat center center fixed"); 
         // swap out small displays for xs displays on smaller screens
         $('.display').removeClass('XS');
         $('.display').addClass('S');
         $('.flapper').css('font-weight', 800);
       }
       else if(window.matchMedia(imgsmall).matches) {
-        $('.change img').each(function () {
-            this.src= "/assets/background1-a9d15ff618c71c16cc1cc81dc8d8f2f1.jpg";
-
-        });
+        $('.full-screen-img').css("background-image", "url(/assets/background1-a9d15ff618c71c16cc1cc81dc8d8f2f1.jpg) no-repeat center center fixed"); 
         // swap out small displays for xs displays on smaller screens
         $('.display').removeClass('S');
         $('.display').addClass('XS');
