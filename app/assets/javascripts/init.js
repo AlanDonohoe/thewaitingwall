800ae5a62d6b42d0b277294f67cb7f42164132d3
Alan Donohoe <alandonohoe@hotmail.com>
2015-09-03 05:24:25 +0100

    reinstate image swapping based on screen size in init.js

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,5 +1,10 @@
 (function($){
-  $(window).load(function() {  document.getElementById("hideAll").style.display = "none"; });
+  $(window).load(function() {
+    var loadingScreen = document.getElementById("hideAll");
+    if(!!loadingScreen){
+      loadingScreen.style.display = "none";
+    }
+  });
   $(function(){
     var calledOnce = false;
     (function hide_post_msg_btn() {
@@ -20,14 +25,22 @@
     var imgbig   = "(min-device-width:960px)";
     function imageresize() {
       if(window.matchMedia(imgbig).matches) {
-        $('.full-screen-img').css("background-image", "url(/assets/background2-6492c83fc5943157d9debcd80089e7f7.jpg) no-repeat center center fixed"); 
+        // the wall
+        $('.full-screen-img').css("background-image", "url(/assets/background2-6492c83fc5943157d9debcd80089e7f7.jpg) no-repeat center center fixed");
+        $('.change img').each(function () {
+          this.src= "/assets/background2-6492c83fc5943157d9debcd80089e7f7.jpg";
+        });
         // swap out small displays for xs displays on smaller screens
         $('.display').removeClass('XS');
         $('.display').addClass('S');
         $('.flapper').css('font-weight', 800);
       }
       else if(window.matchMedia(imgsmall).matches) {
-        $('.full-screen-img').css("background-image", "url(/assets/background1-a9d15ff618c71c16cc1cc81dc8d8f2f1.jpg) no-repeat center center fixed"); 
+        // the wall
+        $('.full-screen-img').css("background-image", "url(/assets/background1-a9d15ff618c71c16cc1cc81dc8d8f2f1.jpg) no-repeat center center fixed");
+        $('.change img').each(function () {
+          this.src= "/assets/background1-a9d15ff618c71c16cc1cc81dc8d8f2f1.jpg";
+        });
         // swap out small displays for xs displays on smaller screens
         $('.display').removeClass('S');
         $('.display').addClass('XS');
