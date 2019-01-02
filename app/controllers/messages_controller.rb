1bfcd48d43518ef95209e7291d055e3ee24b6ab7
Alan Donohoe <alandonohoe@hotmail.com>
2019-01-02 00:37:36 +0000

    Remove logging

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -30,12 +30,7 @@ class MessagesController < ApplicationController
   # POST /messages.json
   def create
     # TODO delete approved params - so no one can hack in an approved message
-    puts 'current_tenant ' + current_tenant.inspect
-    puts 'batches count ' + current_tenant.batches.count.inspect
-
     @message = current_tenant.messages.new(message_params)
-
-    puts '@message ' + @message.inspect
 
     respond_to do |format|
       if @message.save
